a411cb8b73faba3e56ab09bed135b61bc3b59815
turik777 <52492191+turik777@users.noreply.github.com>
2025-03-09 23:16:31 +0300

    feat: complete sortCitiesArray function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -241,8 +241,12 @@ function fromJSON(proto, json) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => {
+    return a.country === b.country
+      ? a.city.charCodeAt() - b.city.charCodeAt()
+      : a.country.charCodeAt() - b.country.charCodeAt();
+  });
 }
 
 /**
